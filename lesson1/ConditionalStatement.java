a0158cbf5738b22fae64a2e0fc036dec3337f047
improcoder <improcoder>
2020-06-09 02:41:16 +0300

    lesson1_hw_2_change_word

--- lesson1/ConditionalStatement.java
+++ lesson1/ConditionalStatement.java
@@ -1,28 +1,28 @@
 public class ConditionalStatement {
 	public static void main(String[] args){
 	int age = 25;
-	String sex = "man";
+	String sex = "Man";
 	double height = 1.85;
 	String nameWord = "M";
 
 	if(age > 20)
 		System.out.println("Your age > 20");
 
-	if(sex.equals("man"))
-		System.out.println("You are Man");
+	if(sex.equals("Man"))
+		System.out.println("You are Man!");
 	
-	if(!sex.equals("man"))
-		System.out.println("You are not Man");
+	if(!sex.equals("Man"))
+		System.out.println("You are not Man!");
 
 	if(height < 1.80)
-		System.out.println("You are Football-Man");
+		System.out.println("You are Football-Man!");
 	else
-		System.out.println("You are Basketball-Man");
+		System.out.println("You are Basketball-Man!");
 
 	if(nameWord.equals("M"))
-		System.out.println("May be you are Matvey");
+		System.out.println("May be you are Matvey?");
 	else if(nameWord.equals("N"))
-		System.out.println("May be you are Nikita");
+		System.out.println("May be you are Nikita?");
 	else
 		System.out.println("Who are you Man?");
 	}
