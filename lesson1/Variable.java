d57e8d6b17bee18445a8cad5df2da020b2623376
improcoder <improcoder>
2020-06-09 04:00:11 +0300

    lesson1_hw_1

---
+++ lesson1/Variable.java
@@ -0,0 +1,13 @@
+
+public class Variable {	
+    public static void main(String[] args) {
+        byte cpCore = 4; 
+        short hddGb = 500;
+        int cpMegaHertz = 2500;
+        long cpHertz = 250000000;
+        float cpGhz = 2.5f;
+        double ssdMhz = 2.500;
+        char cpModel = 'G';
+        boolean fast = true;
+    }
+}
