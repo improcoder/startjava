106f0bbafb0bbb3a3d3b1742083bf101afebe998
improcoder <improcoder>
2020-06-13 01:22:01 +0300

    class rename, logic change, code refactor

--- lesson1/GuessNumber.java
+++ lesson1/GuessNumber.java
@@ -1,19 +1,19 @@
-public class GuessTheNumber{
-	public static void main(String[] args){
+public class GuessNumber {
+	public static void main(String[] args) {
+		
 		int computerNum = 25;
-		int playerNum = 25;
+		int playerNum = 0;
 
-		while(true){
-			if(playerNum > 100 || playerNum < 0){
+		while (true) {
+			if (playerNum > 100 || playerNum < 0) {
 				System.out.println("Your number is not from 0 to 100. Try again!");
-			}
-			else if(playerNum > computerNum){
+			} else if (playerNum > computerNum) {
 				System.out.println("Your number is too big. Try again!");
-			}
-			else if(playerNum < computerNum){
+				playerNum--;
+			} else if (playerNum < computerNum) {
 				System.out.println("Your number is too small. Try again!");
-			}
-			else{
+				playerNum += 2;
+			} else {
 				System.out.println("You guessed the number!");
 				break;
 			}
