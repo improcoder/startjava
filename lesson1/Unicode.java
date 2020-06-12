584ef9c01b444cfdbd5136e53bd0fcd05fa8e531
improcoder <improcoder>
2020-06-12 23:35:27 +0300

    change name of variable, code refactor

--- lesson1/Unicode.java
+++ lesson1/Unicode.java
@@ -1,7 +1,7 @@
-public class Unicode{
-	public static void main(String[] args){
-		for (char uni = 33; uni <= 126; uni++){
-			System.out.println(uni);
+public class Unicode {
+	public static void main(String[] args) {
+		for (char symbol = 33; symbol <= 126; symbol++) {
+			System.out.println(symbol);
 		}
 	}
 }
