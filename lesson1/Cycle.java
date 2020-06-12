c5526d0978d5a3a5a0394c69cd02407fde9f5667
improcoder <improcoder>
2020-06-12 23:32:05 +0300

    code refactor

--- lesson1/Cycle.java
+++ lesson1/Cycle.java
@@ -1,25 +1,23 @@
-public class Cycle{
-	public static void main(String[] args){
-		for (int i = 0; i <= 20; i++){
+public class Cycle {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 20; i++) {
 			System.out.println(i);
 		}
 
 		int counter1 = 6; 
-		while (counter1 >= -6){
+		while (counter1 >= -6) {
 			System.out.println(counter1);
 			counter1 -= 2;
 		}
 
 		int counter2 = 10;
 		int result = 0;
-
-		do{
+		do {
 			counter2++;
-			if(counter2 % 2 != 0){
+			if (counter2 % 2 != 0) {
 				result += counter2;
 			}
-		} while(counter2 < 20);
-
+		} while (counter2 < 20);
 		System.out.println(result);
 	}
 }
