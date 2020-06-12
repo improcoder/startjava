a74bd19452baa3e40d714f97210f74c8014b2f36
improcoder <improcoder>
2020-06-12 23:24:25 +0300

    print values of variables, change the variable names

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -1,13 +1,22 @@
 
 public class Variable {	
     public static void main(String[] args) {
-        byte cpCore = 4; 
-        short hddGb = 500;
-        int cpMegaHertz = 2500;
-        long cpHertz = 250000000;
-        float cpGhz = 2.5f;
-        double ssdMhz = 2.500;
-        char cpModel = 'G';
-        boolean fast = true;
+        byte cpuCores = 4; 
+        short graphicsCardGb = 2;
+        int ssdWrite = 350;
+        long ssdRead = 550;
+        float ssdFormFactor = 2.5f;
+        double cpuGhz = 2.55;
+        char cpuModel = 'G';
+        boolean isComputerFast = true;
+
+        System.out.println(cpuCores);
+        System.out.println(graphicsCardGb);
+        System.out.println(ssdWrite);
+        System.out.println(ssdRead);
+        System.out.println(ssdFormFactor);
+        System.out.println(cpuGhz);
+        System.out.println(cpuModel);
+        System.out.println(isComputerFast);
     }
 }
