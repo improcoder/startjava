3ef2c5392bc2ccc263476f3c6b2a9ecb8351755c
improcoder <improcoder>
2020-08-14 06:53:32 +0300

    hw2_get_set_methods

--- lesson2/Wolf.java
+++ lesson2/Wolf.java
@@ -1,10 +1,54 @@
 public class Wolf {
 
-	String name = "Wolf";
-	String color = "Grey";
-	int age = 5;
-	double weight = 25.5;
-	char sex = 'M';
+	private String name = "Wolf";
+	private String color = "Gray";
+	private int age = 5;
+	private double weight = 25.5;
+	private char sex = 'M';
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age > 8) {
+			System.out.println("Wrong age");
+		} else {
+			this.age = age;
+		}
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public char getSex() {
+		return sex;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
 
 	public void move() {
 
--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -3,12 +3,18 @@ public class WolfTest {
 	public static void main(String[] args) {
 		
 		Wolf wolf = new Wolf();
+
+		wolf.setName("Gray Wolf");
+		wolf.setColor("Darkgray");
+		wolf.setAge(3);
+		wolf.setWeight(21.5);
+		wolf.setSex('M');
 		
-		System.out.println(wolf.name);
-		System.out.println(wolf.color);
-		System.out.println(wolf.age);
-		System.out.println(wolf.weight);
-		System.out.println(wolf.sex);
+		System.out.println(wolf.getName());
+		System.out.println(wolf.getColor());
+		System.out.println(wolf.getAge());
+		System.out.println(wolf.getWeight());
+		System.out.println(wolf.getSex());
 
 		wolf.move();
 		wolf.sit();
